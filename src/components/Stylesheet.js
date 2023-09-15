be5cb76117acedacfea1692c208d41abf48f8b88
“LeslieAine” <aineleslie@gmail.com>
2023-09-15 07:04:51 -0700

    basic styles

---
+++ src/components/Stylesheet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './myStyles.css'
+
+function Stylesheet() {
+  return (
+    <div>
+        <h2 className='primary'>Stylesheets</h2>
+    </div>
+  )
+}
+
+export default Stylesheet
