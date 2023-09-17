535dcaaaf043a367350f6fd67317a2830fabd9d5
“LeslieAine” <aineleslie@gmail.com>
2023-09-17 14:09:27 -0700

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import Greet from "./components/Greet";
-import Welcome from "./components/Welcome";
-import FunctionClick from "./components/FunctionClick";
-import ClassClick from "./components/ClassClick";
-import EventBind from "./components/EventBind";
-import ParentComponent from "./components/ParentComponent";
-import UserGreeting from "./components/UserGreeting";
-import NameList from "./components/PersonList";
-import Stylesheet from "./components/Stylesheet";
-import Form from "./components/Form";
+import React from 'react'
+import FragmetsDemo from './components/FragmetsDemo';
+import Table from './components/Table';
+
 
 function App() {
   return (
@@ -21,7 +15,9 @@ function App() {
       {/* <UserGreeting /> */}
       {/* <NameList /> */}
       {/* <Stylesheet /> */}
-      <Form />
+      {/* <Form /> */}
+      {/* <FragmetsDemo /> */}
+      <Table />
     </div>
   );
 }
