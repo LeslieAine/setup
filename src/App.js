38e82702a191ee1acc036a5ad409a7a24441d677
“LeslieAine” <aineleslie@gmail.com>
2023-09-18 13:36:38 -0700

    clear boilerplate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css'
-import FRInputParent from './components/FRInputParent';
-// import FocusInput from './components/FocusInput';
-// import RefsDemo from "./components/RefsDemo";
+
 
 function App() {
   return (
     <div className="App">
-      {/* <FocusInput/> */}
-      <FRInputParent />
+      
     </div>
   );
 }
