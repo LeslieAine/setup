44ca3477a2e95678adebb7cdaccb737dbaf1a8e2
“LeslieAine” <aineleslie@gmail.com>
2023-09-18 11:30:47 -0700

    Add new components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import Greet from "./components/Greet";
-import Welcome from "./components/Welcome";
-import FunctionClick from "./components/FunctionClick";
-import ClassClick from "./components/ClassClick";
-import EventBind from "./components/EventBind";
-import ParentComponent from "./components/ParentComponent";
-import UserGreeting from "./components/UserGreeting";
-import NameList from "./components/PersonList";
-import Stylesheet from "./components/Stylesheet";
-import Form from "./components/Form";
+import ParentComp from "./components/ParentComp";
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet name = 'John' userName= 'Superman'/> */}
-      {/* <Welcome name = 'leslie' userName = 'magician'/>  */}
-      {/* <FunctionClick /> */}
-      {/* <ClassClick /> */}
-      {/* <EventBind /> */}
-      {/* <ParentComponent /> */}
-      {/* <UserGreeting /> */}
-      {/* <NameList /> */}
-      {/* <Stylesheet /> */}
-      <Form />
+      <ParentComp />
     </div>
   );
 }
