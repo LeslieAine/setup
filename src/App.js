430f0c1cb9a90b3a7e71da8d2c00e7763d01b3d1
“LeslieAine” <aineleslie@gmail.com>
2023-09-18 12:42:32 -0700

    New components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import './App.css'
+import FRInputParent from './components/FRInputParent';
+// import FocusInput from './components/FocusInput';
+// import RefsDemo from "./components/RefsDemo";
 
 function App() {
   return (
     <div className="App">
-      
+      {/* <FocusInput/> */}
+      <FRInputParent />
     </div>
   );
 }
