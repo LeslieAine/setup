7313cfd5efcfcd3439b5dd80b52d53a69236519d
“LeslieAine” <aineleslie@gmail.com>
2023-09-19 12:39:25 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css'
+import ClickCounterTwo from './components/ClickCounterTwo';
+import Counter from './components/Counter';
+import HoverCounterTwo from './components/HoverCounterTwo';
+import User from './components/User';
 
 
 function App() {
   return (
     <div className="App">
-      
+      {/* <ClickCounterTwo />
+      <HoverCounterTwo />
+      <User render= {(isLoggedIn) => isLoggedIn ? 'leslie' : 'guest'}/> */}
+      <Counter 
+      render = {(count, incrementCount) => (
+      <ClickCounterTwo count ={count} incrementCount= {incrementCount}/>
+      ) }/>
+      <Counter 
+      render = {(count, incrementCount) => (
+      <HoverCounterTwo count ={count} incrementCount= {incrementCount}/>
+      ) }/>
     </div>
   );
 }
