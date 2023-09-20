6edf992539614f3e9b4ff688b099a235cbdfdec7
“LeslieAine” <aineleslie@gmail.com>
2023-09-20 04:05:43 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css'
-
+import PostForm from './components/PostForm';
+// import PostList from './components/PostList';
 
 function App() {
   return (
     <div className="App">
-      
+      {/* <PostList /> */}
+      <PostForm />
     </div>
   );
 }
 
 export default App;
+
+//going to use jsonplaceholder to test the api and http requests
